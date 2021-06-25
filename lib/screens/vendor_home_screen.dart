2f366c69306084c56c8e7712c124d9f778d17f14
Ho Quang <19520875@gm.uit.edu.vn>
2021-06-25 21:43:08 +0700

    add category to vendor

--- lib/screens/vendor_home_screen.dart
+++ lib/screens/vendor_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app_flutter/widgets/categories_widget.dart';
 import 'package:grocery_app_flutter/widgets/vendor_appbar.dart';
 import 'package:grocery_app_flutter/widgets/vendor_banner.dart';
 import 'package:provider/provider.dart';
@@ -22,6 +23,7 @@ class VendorHomeScreeen extends StatelessWidget {
         body: Column(
           children: [
            VendorBanner(),
+            Expanded(child: VendorCategories()),
           ],
         ),
       ),
